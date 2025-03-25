082dcceaec11943a34e4c6fac63805f933eed8a5
benjamin846 <57553611+benjamin846@users.noreply.github.com>
2025-03-26 10:01:46 +1100

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -1,10 +1,20 @@
+import numpy as np
+# Nural network var
+inputVector = np.array([1.66, 1.56])
+wheights1 = np.array([1.45, -0.66])
+bias = np.array([0.0])
+
 prompt = input("> ")
 
-def submitPrompt(prompt):
+def sigmoid(x):
+  return 1 / (1 + np.exp(-x))
+
+def submitPrompt(prompt, inputVecters, weights, bias):
+  layer1 = np.dot(inputVecters, weights) + bias
   if prompt:
     pass
   else:
     print("No prompt provided")
     submitPrompt(prompt)
 
-submitPrompt(prompt)
+submitPrompt(prompt, inputVector, weights1, bias)
