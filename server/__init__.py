9c9d8c3580185cd99da0e7f305666cb10b220e85
benjamin846 <57553611+benjamin846@users.noreply.github.com>
2025-03-25 13:25:59 +1100

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -1,1 +1,36 @@
-print("Hello World")
+import socket
+import threading
+
+PORT = 5050
+HEADER = 64
+SERVER = socket.gethostbyname(socket.gethostname)
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MSG = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handleClient(conn, addr):
+    print(f"New connection: {addr}")
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+        msg =  conn.recv(msg_length).decode(FORMAT)
+        if msg == DISCONNECT_MSG:
+            connected = False
+        print(f"[{addr}]: {msg}")
+    conn.close()
+
+def start():
+    server.listen()
+    print(f"Server is Listening on Port {PORT}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handleClient, args=(conn, addr))
+        thread.start()
+        print(f"THREAD: Active Clients: {threading.activeCount() - 1}")
+
+print("[INIT] Server is Starting... ")
+start()
