7e565446cb4f73da8357fe24b442a0e7de3a127e
benjamin846 <57553611+benjamin846@users.noreply.github.com>
2025-03-26 17:05:34 +1100

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -1,20 +1,49 @@
 import numpy as np
-# Nural network var
-inputVector = np.array([1.66, 1.56])
-wheights1 = np.array([1.45, -0.66])
-bias = np.array([0.0])
+inputVector = [1.72, 1.23]
 
-prompt = input("> ")
+class NeuralNetwork:
+    def __init__(self, learning_rate):
+        self.weights = np.array([np.random.randn(), np.random.randn()])
+        self.bias = np.random.randn()
+        self.learning_rate = learning_rate
 
-def sigmoid(x):
-  return 1 / (1 + np.exp(-x))
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
 
-def submitPrompt(prompt, inputVecters, weights, bias):
-  layer1 = np.dot(inputVecters, weights) + bias
-  if prompt:
-    pass
-  else:
-    print("No prompt provided")
-    submitPrompt(prompt)
+    def _sigmoid_deriv(self, x):
+        return self._sigmoid(x) * (1 - self._sigmoid(x))
 
-submitPrompt(prompt, inputVector, weights1, bias)
+    def predict(self, input_vector):
+        layer_1 = np.dot(input_vector, self.weights) + self.bias
+        layer_2 = self._sigmoid(layer_1)
+        prediction = layer_2
+        return prediction
+
+    def _compute_gradients(self, input_vector, target):
+        layer_1 = np.dot(input_vector, self.weights) + self.bias
+        layer_2 = self._sigmoid(layer_1)
+        prediction = layer_2
+
+        derror_dprediction = 2 * (prediction - target)
+        dprediction_dlayer1 = self._sigmoid_deriv(layer_1)
+        dlayer1_dbias = 1
+        dlayer1_dweights = (0 * self.weights) + (1 * input_vector)
+
+        derror_dbias = (
+            derror_dprediction * dprediction_dlayer1 * dlayer1_dbias
+        )
+        derror_dweights = (
+            derror_dprediction * dprediction_dlayer1 * dlayer1_dweights
+        )
+
+        return derror_dbias, derror_dweights
+
+    def _update_parameters(self, derror_dbias, derror_dweights):
+        self.bias = self.bias - (derror_dbias * self.learning_rate)
+        self.weights = self.weights - (
+            derror_dweights * self.learning_rate
+        )
+
+learning_rate = 0.1
+network = NeuralNetwork(learning_rate)
+network.predict(inputVector)
